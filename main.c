a730936b8b711755983b333031f589dc6fb7e78a
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-12-14 03:40:12 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,13 @@
 #include "monty.h"
 
+
+void push(Stack *stack, int value) {
+    if (stack->top == MAX_STACK_SIZE) {
+        fprintf(stderr, "Error: malloc failed\n");
+        exit(EXIT_FAILURE);
+    }
+    stack->stack[stack->top++] = value;
+}
 int main(int argc, char *argv[]) {
     char line[256];
      char *file_path = argv[1];
