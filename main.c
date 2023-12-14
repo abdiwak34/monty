bce9c67b81563e78142ce51f1dec067012f2cdf5
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-12-14 09:58:02 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,82 +1,47 @@
 #include "monty.h"
 
+bus_t bus = {NULL, NULL, NULL, 0};
 
-void push(Stack *stack, int value, int line_number) {
-	(void)line_number;
-    if (stack->top == MAX_STACK_SIZE) {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-    stack->stack[stack->top++] = value;
-}
-void pint(const Stack *stack, int line_number) {
-    if (stack->top == 0) {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    printf("%d\n", stack->stack[stack->top - 1]);
-}
-void pop(Stack *stack, int line_number) {
-    if (stack->top == 0) {
-        fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    stack->top--;
-}
-int main(int argc, char *argv[]) {
-    char line[256];
-    int line_number = 0;
-    char opcode[16];
-    int argument;
-     char *file_path = argv[1];
-    FILE *file = fopen(file_path, "r");
-     Stack stack;
-    stack.top = 0;
-  
-    if (argc != 2) {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+/**
+ * main - monty code interpreter
+ * @argc: number of arguments
+ * @argv: monty file location
+ * Return: 0 on success
+ */
+int main(int argc, char *argv[])
+{
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
-   
-    if (file == NULL) {
-        fprintf(stderr, "Error: Can't open file %s\n", file_path);
-        exit(EXIT_FAILURE);
-    }
-
-   
-
-    while (fgets(line, sizeof(line), file) != NULL) {
-        line_number++;
-
-        line[strcspn(line, "\n")] = '\0';
-
-        if (strlen(line) == 0) {
-            continue;
-        }
-	if (strcmp(line, "pint") == 0) {
-   	 pint(&stack, line_number);
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
 	}
-	    if (strcmp(line, "pop") == 0) {
-    pop(&stack, line_number);
-}
-       
-
-        if (sscanf(line, "%15s %d", opcode, &argument) == 2) {
-            if (strcmp(opcode, "push") == 0) {
-                push(&stack, argument, line_number);
-            } else {
-                fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-                exit(EXIT_FAILURE);
-            }
-        } else if (strcmp(line, "pall") == 0) {
-            pall(&stack);
-        } else {
-            fprintf(stderr, "L%d: invalid instruction: %s\n", line_number, line);
-            exit(EXIT_FAILURE);
-        }
-    }
-
-    fclose(file);
-    return 0;
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+		{
+			execute(content, &stack, counter, file);
+		}
+		free(content);
+	}
+	free_stack(stack);
+	fclose(file);
+	return (0);
 }
