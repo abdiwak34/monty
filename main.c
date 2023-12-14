b9123c9d01e35c871ec33a4d2da76130203bc96f
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-12-14 03:36:12 -0800

    Update main.c

--- main.c
+++ main.c
@@ -1,37 +1,38 @@
 #include "monty.h"
 
 int main(int argc, char *argv[]) {
+     char *file_path = argv[1];
+    FILE *file = fopen(file_path, "r");
+     Stack stack;
+    stack.top = 0;
+
+    char line[256];
+    int line_number = 0;
+     char opcode[16];
+        int argument;
     if (argc != 2) {
         fprintf(stderr, "USAGE: monty file\n");
         exit(EXIT_FAILURE);
     }
 
-    char *file_path = argv[1];
-    FILE *file = fopen(file_path, "r");
+   
     if (file == NULL) {
         fprintf(stderr, "Error: Can't open file %s\n", file_path);
         exit(EXIT_FAILURE);
     }
 
-    Stack stack;
-    stack.top = 0;
-
-    char line[256];
-    int line_number = 0;
+   
 
     while (fgets(line, sizeof(line), file) != NULL) {
         line_number++;
 
-        // Remove trailing newline character
         line[strcspn(line, "\n")] = '\0';
 
-        // Skip empty lines
         if (strlen(line) == 0) {
             continue;
         }
 
-        char opcode[16];
-        int argument;
+       
 
         if (sscanf(line, "%15s %d", opcode, &argument) == 2) {
             if (strcmp(opcode, "push") == 0) {
