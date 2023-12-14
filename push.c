15de67ee87a3c9834a63145e6f0dcea53ab0b0fe
bereket-kume <135522204+bereket-kume@users.noreply.github.com>
2023-12-14 03:32:48 -0800

    Update push.c

--- push.c
+++ push.c
@@ -1,6 +1,6 @@
 #include "monty.h"
 
-void push(Stack *stack, int value, int line_number) {
+void push(Stack *stack, int value) {
     if (stack->top == MAX_STACK_SIZE) {
         fprintf(stderr, "Error: malloc failed\n");
         exit(EXIT_FAILURE);
